0d1341cabd002463dbe3b25795e619d423b0735c
jevans72 <31170086+jevans72@users.noreply.github.com>
2017-08-19 18:31:15 -0500

    A demo to use to help learn open source C# and Repo's

---
+++ Practice/hwapp/Program.cs
@@ -0,0 +1,18 @@
+﻿/**
+* C# demo made for James Evans using .NET Core 2.0
+* By: Michael Evans on 8/19/2017
+* https://www.microsoft.com/net/core#windowscmd
+**/
+
+using System;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
